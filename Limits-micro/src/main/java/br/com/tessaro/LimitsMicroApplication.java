b9f872bc796caf09e03305e3cf04b6f6fb2e8058
Cassio <cassioate@gmail.com>
2022-03-06 03:29:44 -0300

    add limits-config-server.properties

---
+++ Limits-micro/src/main/java/br/com/tessaro/LimitsMicroApplication.java
@@ -0,0 +1,13 @@
+package br.com.tessaro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimitsMicroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LimitsMicroApplication.class, args);
+	}
+
+}
---
+++ Limits-micro/src/main/java/br/com/tessaro/config/Configuration.java
@@ -0,0 +1,14 @@
+package br.com.tessaro.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+
+@Data
+@Component
+@ConfigurationProperties("limits-service")
+public class Configuration {
+	private int minimum;
+	private int maximum;
+}
