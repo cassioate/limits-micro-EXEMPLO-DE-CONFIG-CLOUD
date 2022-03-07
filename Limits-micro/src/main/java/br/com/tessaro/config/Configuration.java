d8f5779afe845fe7d71ea408c1060103e1b840bf
Cassio <cassioate@gmail.com>
2022-03-07 13:55:31 -0300

    Ajuste2

--- Limits-micro/src/main/java/br/com/tessaro/config/Configuration.java
+++ Limits-micro/src/main/java/br/com/tessaro/config/Configuration.java
@@ -7,7 +7,7 @@ import lombok.Data;
 
 @Data
 @Component
-@ConfigurationProperties("limits2-service")
+@ConfigurationProperties("limits3-service")
 public class Configuration {
 	private int minimum;
 	private int maximum;
