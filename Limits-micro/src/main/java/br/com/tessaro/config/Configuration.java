bedf4e47e30690256d74652861dca81e3fa15165
Cassio <cassioate@gmail.com>
2022-03-07 14:01:11 -0300

    Entendendo a diferença do spring.application.name e do @configurationProperties(limits-service)

--- Limits-micro/src/main/java/br/com/tessaro/config/Configuration.java
+++ Limits-micro/src/main/java/br/com/tessaro/config/Configuration.java
@@ -7,7 +7,9 @@ import lombok.Data;
 
 @Data
 @Component
-@ConfigurationProperties("limits3-service")
+// Configuração abaixo é refernte ao limits-service.minimum=55 de dentro do limits-service.properties
+// Após ter escolhido o arquvio que será lido através do "spring.application.name" dentro do .properties, aqui iremos escolher qual a "variavel" que será lida, no caso será toda aqulea com o inicio limits-service
+@ConfigurationProperties("limits-service")
 public class Configuration {
 	private int minimum;
 	private int maximum;
